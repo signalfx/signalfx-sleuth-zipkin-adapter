9e4d118147761edde9d7840466db224d651b5eab
Brandon Currie <bcurrie@signalfx.com>
2019-10-03 17:40:00 -0700

    Adding configuration for Spring 1.0.6

---
+++ src/test/java/com/signalfx/tracing/extensions/sleuth/SignalFxZipkinPropertiesTest.java
@@ -0,0 +1,28 @@
+package com.signalfx.tracing.extensions.sleuth;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+public class SignalFxZipkinPropertiesTest {
+
+    SignalFxZipkinProperties properties;
+    
+    @Before
+    public void init() {
+	this.properties = new SignalFxZipkinProperties();
+    }
+    
+    @Test
+    public void defaultUrlIsDefaultSignalFxAgent() {
+	Assert.isTrue(this.properties.getBaseUrl().equals("http://localhost:9080"), "Default url is not the SignalFx Smart Agent default");
+    }
+    
+    @Test
+    public void baseUrlDefaultCanBeOverriden() {
+	String newValue = "http://somewhereelse.com:80";
+	this.properties.setBaseUrl(newValue);
+	Assert.isTrue(this.properties.getBaseUrl().equals(newValue), "Unable to successfully set a new baseUrl value in SignalFxZipkinProperties");
+    }
+
+}
